e84685e780f4f87d6c9f6be5a90b708237f60f78
Dag Evensberget <dag.evensberget@gmail.com>
2022-10-13 11:36:20 +0200

    More 3d reader testing

--- tests/test_reader_3d.py
+++ tests/test_reader_3d.py
@@ -1,5 +1,3 @@
-from typing import Any
-
 import numpy as np
 import matplotlib.pyplot as plt
 from matplotlib import tri
@@ -10,6 +8,8 @@ from contextlib import contextmanager
 from starwinds_readplt.read_dat import read_dat
 from starwinds_readplt.read_plt import read_plt
 
+import pytest
+
 
 @contextmanager
 def catchtime() -> float:
@@ -17,15 +17,22 @@ def catchtime() -> float:
     yield lambda: perf_counter() - start
 
 
-def test_read_3d_dat():
-    filename = "examples/3d__var_1_n00000000.dat"
-    points, corners, aux, title, variables, zone_name = read_dat(filename)
+@pytest.mark.parametrize(
+    "filename", ("examples/3d__var_1_n00000000.dat", "examples/3d__var_1_n00000000.plt")
+)
+def test_read_3d(filename):
+    if filename.split(".")[-1] == "dat":
+        points, corners, aux, title, variables, zone_name = read_dat(filename)
+    else:
+        points, corners, aux, title, variables, zone_name = read_plt(filename)
 
     assert np.min(corners) == 0
     assert np.max(corners) + 1 == points.shape[0]
 
-    assert points.shape == (12800, 24)
-    assert corners.shape == (12288, 8)
+    assert corners.shape[1] == 8
+
+    assert points.shape == (12800, 24)  # This will not apply to other datasets.
+    assert corners.shape == (12288, 8)  # This will not apply to other datasets.
 
 
 def test_compare_dat_and_plt():
@@ -59,6 +66,6 @@ def test_3d_point_cloud():
     axs[0, 0].plot(x, y, ".")
     axs[0, 1].plot(x, z, ".")
     axs[1, 0].plot(y, z, ".")
-    # axs[1,1].plot(x, y, ',')
-    plt.show()
+    axs[1, 1].plot(np.sort((x**2 + y**2 + z**2) ** 0.5), ".")
+    plt.savefig("test_3d_point_cloud.png")
     return fig, axs
