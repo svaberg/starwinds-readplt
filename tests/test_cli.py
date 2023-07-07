7957c106d0792fedd9cb94e5f136c231871fcc0f
Dag <dag.evensberget@gmail.com>
2023-07-07 11:43:01 +0200

    Pass scriptrunner command as a single list

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -3,5 +3,5 @@ import pytest
 
 @pytest.mark.parametrize("ext", ("dat", "plt"))
 def test_dat(script_runner, ext):
-    ret = script_runner.run("sw-quick", "x=0_var_2_n00000000." + ext, cwd="examples")
+    ret = script_runner.run(["sw-quick", "x=0_var_2_n00000000." + ext], cwd="examples")
     assert ret.success
